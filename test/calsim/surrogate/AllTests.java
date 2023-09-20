6ecadd89dfc6d3b5c9757db814eeba9a055b8c74
Eli Ateljevich <Eli.Ateljevich@water.ca.gov>
2023-09-20 13:25:33 -0700

    Imports and syntax

--- test/calsim/surrogate/AllTests.java
+++ test/calsim/surrogate/AllTests.java
@@ -4,8 +4,11 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@SelectClasses({ AggregateMonthsTest.class, CircleLineSearchableTest.class, ConservativeSplineTest.class,
-		DisaggregateMonthsTest.class, MockSurrogateTest.class, TensorCalsimTest.class })
+@SelectClasses({ AggregateMonthsTest.class, CircleLineSearchableTest.class, 
+	             ConservativeSplineTest.class,
+		         DisaggregateMonthsTest.class, 
+		         MockSurrogateTest.class, 
+		         TensorCalsimTest.class })
 public class AllTests {
 
 }
