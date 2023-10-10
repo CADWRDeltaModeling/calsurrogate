206eb0116ff6c676c16060af3552c8d3103092e4
Eli <Eli.Ateljevich@water.ca.gov>
2023-10-10 14:02:46 -0700

    Updated Emmaton test

--- test/calsim/surrogate/TensorCalsimTest.java
+++ test/calsim/surrogate/TensorCalsimTest.java
@@ -9,41 +9,15 @@ import java.util.ArrayList;
 
 class TensorCalsimTest {
 
-
-	@Test
-	void testRSAC092() {
-	    String fname = "./ann_calsim-main/RSAC092";
-	    String[] tensorNames =  {"serving_default_input_preprocessed_input:0"};
-	    String[] tensorNamesInt = new String[0];
-	    String outName = "StatefulPartitionedCall:0";
-
-	    
-	    
-	    System.out.println("Success creating wrapper");
-	    long[][] inpInt = {{}};
-
-	    double[][] inpFloat = new double[][] {
-	       { 0.6, 0.72, 0.9, 0.75237382, 0.9,
-	        -0.21546655, -0.17141414, -0.38003647, -0.65384933, -0.77411415,
-	        -0.89288707, -0.69964324, -0.77795826, -0.7921897, -0.81271535,
-	         0., 0., 0., 1., 0., 0., 0., 0., 0., 1.,
-	         0., 0., 0., 0., 0., 0., 0. } };
-	    ArrayList<double[][]> features = new ArrayList<>();
-	    features.add(inpFloat);
-	    ArrayList<long[][]> featuresInt = new ArrayList<>();
-
-	    int feature0=0;
-	    int feature1=0;
-	    int ndx0=1;
-	    int ndx1=0;
-	    Surrogate wrap = new TensorWrapper(fname,tensorNames,tensorNamesInt,outName,null);	
-		
-	}
-
     @Test	
-	void testNDOEstimator() {
-		String fname = "./ann_calsim-main/calsim_delta_outflow_estimator";
-		String[] tensorNames =  {"serving_default_input_preprocessed_input:0"};
+	void testEmmatonANN() {
+		String fname = "./ann_calsim-main/emmaton";
+		String[] tensorNames =  {"serving_default_sac_input:0",
+				"serving_default_exports_input:0","serving_default_dcc_input:0",
+				"serving_default_net_dcd_input:0","serving_default_sjr_input:0",
+				"serving_default_tide_input:0","serving_default_smsg_input:0",
+				};
+		
 		String[] tensorNamesInt = new String[0];
 		String outName = "StatefulPartitionedCall:0";
 
@@ -51,26 +25,34 @@ class TensorCalsimTest {
 		System.out.println("Success creating wrapper");
 		long[][] inpInt = {{}};
 
-		double[][] inpFloat = new double[][] { 
-			{ 0.7228964, 1.05657969, 1.32106489, 0.75237382, 0.86339624,
-				-0.21546655, -0.17141414, -0.38003647, -0.65384933, -0.77411415,
-				-0.89288707, -0.69964324, -0.77795826, -0.7921897, -0.81271535,
-				0., 0., 0., 1., 0., 0., 0., 0., 0., 1.,
-				0., 0., 0., 0., 0., 0., 0. },
-			{ 0.9228964, 1.05657969, 1.32106489, 0.75237382, 0.86339624,
-					-0.21546655, -0.17141414, -0.38003647, -0.65384933, -0.77411415,
-					-0.89288707, -0.69964324, -0.77795826, -0.7921897, -0.81271535,
-					0., 0., 0., 1., 0., 0., 0., 0., 0., 1.,
-					0., 0., 0., 0., 0., 0., 0. } };    
-					ArrayList<double[][]> features = new ArrayList<double[][]>();
-					features.add(inpFloat);
-					ArrayList<long[][]> featuresInt = new ArrayList<long[][]>();
-					float[][] out = wrap.estimate(features, featuresInt);
-                    assertEquals(out[0][0],5216.211,0.001);
-					float[][] out2 = wrap.estimate(features, featuresInt);
-					assertEquals(out[0][0],out2[0][0]); //consistency
+		double[][] sac = {{7724.8,7763.5,7785.7,7799.5,7808.8,7815.2,7819.8,7823.2,7831.7,
+			          7816.5,7737.3,7746.7,8539.2,9661.6,9685.6,10222.1,10596.9,10586.9}};
+		double[][] exp =  {{2972.1,2972.1,2972.1,2972.1,2972.1,2972.1,2972.1,2972.1,2972.1,
+			                   2788.0,1959.6,1959.6,2086.5,2308.6,2308.6,1816.7,1406.7,1406.7}};
 
-	}
-	
-	
+		double[][]	dcc = {{30.0,30.0,30.0,30.0,30.0,30.0,30.0,30.0,30.0,30.2,
+			                31.0,31.0,31.0,31.0,31.0,28.3,26.0,26.0}};
+		double[][] net_dcd = {{1680.3,1680.3,1680.3,1680.3,1680.3,1680.3,1680.3,1680.3,1680.3,
+			                   1835.9,2536.0,2536.0,2645.9,2838.4,2838.4,2750.8,2677.8,2677.8}};
+		double[][] sjr = {{770.7,761.6,756.3,753.0,750.8,749.3,748.1,747.3,744.8,694.5,
+				            538.5,534.8,503.5,459.9,464.2,805.4,1043.6,1053.8}};
+		double[][] tide =	{{6.2,6.1,6.2,6.1,5.7,5.2,4.8,4.6,4.6,5.7,5.2,5.7,6.5,4.9,6.3,6.2,6.0,6.1}};
+		double[][] smscg =	{{0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.2,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0}};		
+			
+		ArrayList<double[][]> floatInput = new ArrayList<double[][]>();
+		floatInput.add(sac);
+		floatInput.add(exp);
+		floatInput.add(dcc);
+		floatInput.add(net_dcd);
+		floatInput.add(sjr);
+		floatInput.add(tide);
+		floatInput.add(smscg);
+
+		float[][] est = wrap.estimate(floatInput,null);
+		for (int ibatch = 0; ibatch<1 ; ibatch++) {
+			for (int jtime = 0; jtime<4; jtime++) {
+				System.out.println("Out " + ibatch + "," + jtime + "  out: " + est[ibatch][jtime]);
+			}
+		}
+  }
 }
